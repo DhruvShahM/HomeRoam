6b011d530447abe6859246cb002a057e0082a4a6
DhruvShah <dhruvshah78223@gmail.com>
2024-06-09 16:50:16 +0530

    Front-end code setup done.

--- HomeRoam-clone-front/src/app/app.component.ts
+++ HomeRoam-clone-front/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from "primeng/button";
+// import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+// import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+// import {NavbarComponent} from "./layout/navbar/navbar.component";
+// import {FooterComponent} from "./layout/footer/footer.component";
+import {ToastModule} from "primeng/toast";
+// import {ToastService} from "./layout/toast.service";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
