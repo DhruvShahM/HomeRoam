ab7a0b9b78d15662698f03a6534c66b3e5d28cb2
DhruvShah <dhruvshah78223@gmail.com>
2024-06-10 17:57:25 +0530

    Front-end necesary components are added.

--- HomeRoam-clone-front/src/app/app.component.ts
+++ HomeRoam-clone-front/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ButtonModule} from "primeng/button";
 // import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
@@ -8,14 +8,26 @@ import {ButtonModule} from "primeng/button";
 import {ToastModule} from "primeng/toast";
 // import {ToastService} from "./layout/toast.service";
 import {MessageService} from "primeng/api";
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'HomeRoam-front';
+export class AppComponent implements OnInit {
+  faIconLibrary=inject(FaIconLibrary);
+
+  ngOnInit(): void {
+      this.initFontAwesome();
+  }
+
+  private initFontAwesome():void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+
 }
