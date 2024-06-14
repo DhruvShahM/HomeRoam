0fb40ad39e422176dacaa13d218919a7e0390098
DhruvShah <dhruvshah78223@gmail.com>
2024-06-14 08:53:03 +0530

    Listing related information now we can save and retrive the records from the backend side.

---
+++ HomeRoam-clone-back/HomeRoam-clone-back/src/main/java/fr/codecake/HomeRoam_clone_back/listing/application/CreatedListingDTO.java
@@ -0,0 +1,5 @@
+package fr.codecake.HomeRoam_clone_back.listing.application;
+
+public record CreatedListingDTO(String publicId) {
+    
+}
--- HomeRoam-clone-back/HomeRoam-clone-back/src/main/java/fr/codecake/HomeRoam_clone_back/listing/mapper/ListingPictureMapper.java
+++ HomeRoam-clone-back/HomeRoam-clone-back/src/main/java/fr/codecake/HomeRoam_clone_back/listing/mapper/ListingPictureMapper.java
@@ -1,38 +1,37 @@
-// package fr.codecake.HomeRoam_clone_back.listing.mapper;
+package fr.codecake.HomeRoam_clone_back.listing.mapper;
 
-// import java.util.List;
-// import java.util.Set;
+import java.util.List;
+import java.util.Set;
 
-// import org.mapstruct.Mapper;
-// import org.mapstruct.Mapping;
-// import org.mapstruct.Named;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import fr.codecake.HomeRoam_clone_back.listing.application.dto.sub.PictureDTO;
+import fr.codecake.HomeRoam_clone_back.listing.domain.ListingPicture;
+
+@Mapper(componentModel = "spring")
+public interface ListingPictureMapper {
 
 
+    Set<ListingPicture> pictureDTOsToListingPictures(List<PictureDTO> pictureDTOs);
 
-// import fr.codecake.HomeRoam_clone_back.listing.domain.ListingPicture;
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "listing", ignore = true)
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
+    @Mapping(target = "cover", source = "isCover")
+    ListingPicture pictureDTOToListingPicture(PictureDTO pictureDTO);
 
-// @Mapper(componentModel = "spring")
-// public interface ListingPictureMapper {
+    List<PictureDTO> listingPictureToPictureDTO(List<ListingPicture> listingPictures);
 
+    @Mapping(target = "isCover", source = "cover")
+    PictureDTO convertToPictureDTO(ListingPicture listingPicture);
 
-//     Set<ListingPicture> pictureDTOsToListingPictures(List<PictureDTO> pictureDTOs);
+    @Named("extract-cover")
+    default PictureDTO extractCover(Set<ListingPicture> pictures) {
+        return pictures.stream().findFirst().map(this::convertToPictureDTO).orElseThrow();
+    }
 
-//     @Mapping(target = "id", ignore = true)
-//     @Mapping(target = "listing", ignore = true)
-//     @Mapping(target = "createdDate", ignore = true)
-//     @Mapping(target = "lastModifiedDate", ignore = true)
-//     @Mapping(target = "cover", source = "isCover")
-//     ListingPicture pictureDTOToListingPicture(PictureDTO pictureDTO);
-
-//     List<PictureDTO> listingPictureToPictureDTO(List<ListingPicture> listingPictures);
-
-//     @Mapping(target = "isCover", source = "cover")
-//     PictureDTO convertToPictureDTO(ListingPicture listingPicture);
-
-//     @Named("extract-cover")
-//     default PictureDTO extractCover(Set<ListingPicture> pictures) {
-//         return pictures.stream().findFirst().map(this::convertToPictureDTO).orElseThrow();
-//     }
-
-// }
+}
 
