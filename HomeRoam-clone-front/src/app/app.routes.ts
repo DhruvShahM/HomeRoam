20a75a5a67a128337b1d9fe53e55674ec741828c
DhruvShah <dhruvshah78223@gmail.com>
2024-06-16 11:29:00 +0530

    Home and reservation pages are added in the front-end app.

--- HomeRoam-clone-front/src/app/app.routes.ts
+++ HomeRoam-clone-front/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { PropertiesComponent } from './landlord/properties/properties.component';
 import { authorityRouteAccess } from './shared/Services/core/auth/authority-route-access';
+import { HomeComponent } from './home/home.component';
+import { DisplayListingComponent } from './tenant/display-listing/display-listing.component';
+import { BookedListingComponent } from './tenant/booked-listing/booked-listing.component';
+import { ReservationComponent } from './landlord/reservation/reservation.component';
 
 export const routes: Routes = [
   {
@@ -11,25 +15,25 @@ export const routes: Routes = [
       authorities: ["ROLE_LANDLORD"]
     }
   },
-//   {
-//     path: '',
-//     component: HomeComponent
-//   },
-//   {
-//     path: 'listing',
-//     component: DisplayListingComponent
-//   },
-//   {
-//     path: "booking",
-//     component: BookedListingComponent
-//   },
-//   {
-//     path: "landlord/reservation",
-//     component: ReservationComponent,
-//     canActivate: [authorityRouteAccess],
-//     data: {
-//       authorities: ["ROLE_LANDLORD"]
-//     }
-//   }
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'listing',
+    component: DisplayListingComponent
+  },
+  {
+    path: "booking",
+    component: BookedListingComponent
+  },
+  {
+    path: "landlord/reservation",
+    component: ReservationComponent,
+    canActivate: [authorityRouteAccess],
+    data: {
+      authorities: ["ROLE_LANDLORD"]
+    }
+  }
 
 ];
